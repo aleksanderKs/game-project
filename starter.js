bc73b27eaa679188b75cf47eb8074a6d76ab8bd7
aleksander kutsenkov <ligalizer8@gmail.com>
2016-06-09 19:58:25 -0400

    javascript part star button user choise

--- starter.js
+++ starter.js
@@ -1,3 +1,37 @@
- $(document).ready(function() {
-   console.log("i' am ready");
+$(document).ready(function() {
+
+var first = "First";
+var second = "Second";
+
+$('.first').click(function( ) {
+  $('p').eq(1).text(" ").append(first);
+
+});
+
+$('.second').click(function() {
+  $('p').eq(1).text(" ").append(second);
+  // $(this).unbind('click');  // click only once on element
+});
+
+
+
+
+ var w = $(window).width();
+  $('#start').click(function() {
+     $('#animal1').animate({
+      left: w
+      },
+      5000, function() {
+
+     });
+
+   $('#animal2').animate({
+       left: w
+       },
+        4000, function() {
+
+     });
+
+    });
  });
+
