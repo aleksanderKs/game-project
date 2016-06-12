f27f2489a521be2d19ff145fbc411d66f8da7228
aleksander kutsenkov <ligalizer8@gmail.com>
2016-06-12 13:04:31 -0400

    issue with place bets function

--- starter.js
+++ starter.js
@@ -9,9 +9,41 @@ $('#start').hover(function(){
 
 
 
-var first ="animalN1";
-var second = "Your animal N2";
-var third="Your animal N3 ";
+
+
+
+// function hey(){
+//  var $a =$('.first');
+// var $b =$('.second');
+// var $c =$('.third');
+
+// $($a).on('click change', function() {
+//     $(this).data('clicked', true);
+//    return true;
+// })
+
+// $($b).on('click change', function() {
+//     $(this).data('clicked', true);
+//     return true;
+// })
+
+// $($c).on('click change', function() {
+//     $(this).data('clicked', true);
+//     console.log('clicked c ');
+//     return true;
+// })
+
+
+// };
+// hey();
+
+
+
+
+
+var first ="anima";
+var second ="animal";
+var third="animalN";
 var animaPicked = "";
 var buttonStart =$('#start');
 
@@ -51,7 +83,11 @@ $('.third').click(function() {
 });
 
 
-var $b = $('#userPick').html();
+
+
+
+
+
 var $winner ="";
 
 
@@ -61,26 +97,60 @@ function enableStart() {
 
   $('#start').click(function() {
 
+
+   var $ch =$('#check').html().length;
+   var $b = $('#userPick').html().length;
+
+
     $('#animal1').animate({left: w}, speed, function() {
-      if(speed <speed2 &&speed <speed3 && first) {
-      console.log("Congratulation you won!!!" + speed);
+      if(speed <speed2 &&speed <speed3 ) {
+     $('#check').text("animal");
+     if( $('#check').html().length === $('#userPick').html().length ) {
+       $('#win').append('Congratulation 1 is won');
+        inputm+=5;
+     }
+
+ }
 
 
-    }
+
+
  // animate function
     }); // end animal 1 animation
      $('#animal2').animate({left: w}, speed2, function() {
-    if(speed2 <speed &&speed2 <speed3 && second) {
-      console.log("Animal 2 is won " + speed2  ) }
+    if (speed2 <speed &&speed2 <speed3 ) {
+$('#check').text("animal2");
+if( $('#check').html().length === $('#userPick').html().length ) {
+       $('#win').append('Congratulation 2 is won');
+        inputm++;
+     };
+
+
+    }
 
     //   // animate function
      }); // end animal 2 animation
 
  $('#animal3').animate({left: w}, speed3, function() {
-if(speed3 <speed2 &&speed3 <speed && third ) {
-      console.log("Congratulation you won" + speed3  ) }
+
+if(speed3 <speed2 &&speed3 <speed   ) {
+$('#check').text("animalN3");
+
+if( $('#check').html().length === $('#userPick').html().length ) {
+       $('#win').append('Congratulation 3rd won');
+       inputm++;
+     };
+
+
+
+}
+
+
+
       // animate function
     }); // end animal 3 animation
+
+
 
 
 
@@ -90,15 +160,58 @@ if(speed3 <speed2 &&speed3 <speed && third ) {
 
 } // end enableStart function
 
-function isBetValid() {
-  // Reads the input value of the betting input box
-  var bets = $('.money').val();
-  // makes sure it is a number
-  // makes sure the gambler has enough money for bet
-  // if all of the above is true return value of true
-  // else return value of false
-}
 
+
+var inputm=0;
+function Tt(){
+  var $b =$('.s');
+$b.click(function(){
+ inputm= $('.money').val();
+
+$(".balance").text(" ").append('<div class="item">' + inputm + '</div>');
+
+
+
+     }
+
+
+
+
+
+)
+ };
+ Tt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function isBetValid() {
+  // Reads the input value of the betting input box
+  // var bets = $('.money').val();
+  // $('.money').append('bets');
+
+
+// $('.money').change(function () {
+//   $('.balance').val($(this).val());
+// });
+
+// // makes sure it is a number
+//   // makes sure the gambler has enough money for bet
+//   // if all of the above is true return value of true
+//   // else return value of false
+// }
+// isBetValid();
 function checkIfwin() {
 // $('.money').keyup(function() {
 //   $(".balance").text($(".money").val());
@@ -106,6 +219,8 @@ function checkIfwin() {
 };
 
 $('#restart').click(function() {
+  $('#start').toggleClass('disabled');
+   $('#check').text(" ");
  $('#animal1').css('left',0);
  $('#animal2').css('left',0);
  $('#animal3').css('left',0);
@@ -115,8 +230,6 @@ var place1 ="First";
 // var place3 ="";
 
 var raceIsfinished ="false";
-
-
 
 
 
